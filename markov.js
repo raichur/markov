01399b8c22c7cffeed16863d34857f286bf570e6
Joshua Raichur <hello@joshuaraichur.com>
2015-07-15 18:10:35 +0530

    more fixes

--- markov.js
+++ markov.js
@@ -3,7 +3,6 @@ var start_words = [], word_stats = {}, terminals = {}, word_list = [];
 
 for (var i = 0; i < word_list.length; i++) {
 	var words = word_list[i].split(' ');
-	console.log(words);
 	terminals[words[words.length-1]] = true;
 	start_words.push(words[0]);
 	for (var j = 0; j < words.length - 1; j++) {
@@ -18,7 +17,6 @@ for (var i = 0; i < word_list.length; i++) {
 var choice = function (a) {
 	var i = Math.floor(a.length * Math.random());
 	return a[i];
-	console.log(a[i]);
 };
 
 var make_title = function (min_length) {
@@ -28,17 +26,17 @@ var make_title = function (min_length) {
 		var next_words = word_stats[word];
 		word = choice(next_words);
 		title.push(word);
-
-		if (title.length > min_length && terminals.hasOwnProperty(word))  break;
+		if (title.length > min_length && terminals.hasOwnProperty(word)) {
+			break;
 		}
+	}
 	if (title.length < min_length) return make_title(min_length);
-
-return title.join(' ');
+	return title.join(' ');
 };
 
 
 $('#render').on('click', function () {
-	word_list = $('#input').val().split(',');
+	word_list = $('#input').val().split(', ');
 	var title = make_title(3 + Math.floor(3 * Math.random()));
 	$('#output').html(title);
 });
