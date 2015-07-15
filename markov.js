127484c02a220da7a9a06c452766af7b52462f07
Joshua Raichur <hello@joshuaraichur.com>
2015-07-15 18:30:54 +0530

    it works!

--- markov.js
+++ markov.js
@@ -1,15 +1,22 @@
-var start_words = [], word_stats = {}, terminals = {}, word_list = [];
+var startwords = []
+var wordstats = {};
+var terminals = {};
 
 
-for (var i = 0; i < word_list.length; i++) {
-	var words = word_list[i].split(' ');
+$('#render').on('click', function () {
+
+	var wordlist = $('#input').val().split(', ');
+	
+	
+	for (var i = 0; i < wordlist.length; i++) {
+	var words = wordlist[i].split(' ');
 	terminals[words[words.length-1]] = true;
-	start_words.push(words[0]);
+	startwords.push(words[0]);
 	for (var j = 0; j < words.length - 1; j++) {
-		if (word_stats.hasOwnProperty(words[j])) {
-			word_stats[words[j]].push(words[j+1]);
+		if (wordstats.hasOwnProperty(words[j])) {
+			wordstats[words[j]].push(words[j+1]);
 		} else {
-			word_stats[words[j]] = [words[j+1]];
+			wordstats[words[j]] = [words[j+1]];
 		}
 	}
 }
@@ -20,10 +27,10 @@ var choice = function (a) {
 };
 
 var make_title = function (min_length) {
-	var word = choice(start_words);
+	var word = choice(startwords);
 	var title = [word];
-	while (word_stats.hasOwnProperty(word)) {
-		var next_words = word_stats[word];
+	while (wordstats.hasOwnProperty(word)) {
+		var next_words = wordstats[word];
 		word = choice(next_words);
 		title.push(word);
 		if (title.length > min_length && terminals.hasOwnProperty(word)) {
@@ -34,9 +41,11 @@ var make_title = function (min_length) {
 	return title.join(' ');
 };
 
-
-$('#render').on('click', function () {
-	word_list = $('#input').val().split(', ');
-	var title = make_title(3 + Math.floor(3 * Math.random()));
+var title = make_title(3 + Math.floor(3 * Math.random()));
 	$('#output').html(title);
+
+
+
+
 });
+
